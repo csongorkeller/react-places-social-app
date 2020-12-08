43a0b35354f5d5fa98ea90e6076b6577a7ddf7ac
Csongor Keller <kellercsongor@gmail.com>
2020-12-08 12:28:59 +0100

    basic user list

--- frontend/src/users/pages/Users.js
+++ frontend/src/users/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h1>Users works!</h1>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Csongor Keller",
+      image:
+        "https://images.unsplash.com/photo-1602524815375-a54449bb00fb?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS}/>;
 };
 
 export default Users;
