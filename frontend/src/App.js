2fc4922a1948b74964a2c9cecc443766f96152a8
Csongor Keller <kellercsongor@gmail.com>
2020-12-08 11:27:57 +0100

    route switching implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import NewPlace from "./places/pages/NewPlace";
 import Users from "./users/pages/Users";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/">
-        <Users />
-      </Route>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <NewPlace />
+        </Route>
+        <Redirect to path="/" />
+      </Switch>
     </Router>
   );
 };
---
+++ frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlace = () => {
+  return <h1> Places also work</h1>;
+};
+
+export default NewPlace;
