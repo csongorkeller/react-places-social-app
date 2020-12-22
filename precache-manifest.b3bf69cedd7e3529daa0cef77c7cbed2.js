80269a14ce2266b55744b90d5fa4f5d2371f1a9f
Csongor Keller <kellercsongor@gmail.com>
2020-12-22 21:02:32 +0100

    Updates

---
+++ precache-manifest.b3bf69cedd7e3529daa0cef77c7cbed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdc57f4043adeb4545818989ea84e6c",
+    "url": "/react-places-social-app/index.html"
+  },
+  {
+    "revision": "9aef47971d91038f1493",
+    "url": "/react-places-social-app/static/css/main.0942250e.chunk.css"
+  },
+  {
+    "revision": "2976a8fafd3f7a72da48",
+    "url": "/react-places-social-app/static/js/2.b9af8fc2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-places-social-app/static/js/2.b9af8fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aef47971d91038f1493",
+    "url": "/react-places-social-app/static/js/main.a63fd06e.chunk.js"
+  },
+  {
+    "revision": "b960376a13dda5fea2db",
+    "url": "/react-places-social-app/static/js/runtime-main.fe0f582a.js"
+  }
+]);
